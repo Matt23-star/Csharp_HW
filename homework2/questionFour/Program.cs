d09613c611816a9214adf4931a975368357e5421
MattFH <2019302100016@whu.edu.cn>
2021-03-05 12:22:46 +0800

    第二次软件构造基础作业

---
+++ homework2/questionFour/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+/*如果矩阵上每一条由左上到右下的对角线上的元素都相同，
+那么这个矩阵是托普利茨矩阵 。
+给定一个 M x N 的矩阵，当且仅当它是托普利茨矩阵时返回 True。*/
+
+namespace questionFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { { 1,2,3,4 },
+                                { 5,1,2,3 },
+                                { 9,5,1,2 }};
+            if (Program.Judge(array))
+            {
+                Console.WriteLine("是托普利茨矩阵");
+            }
+            else
+                Console.WriteLine("不是托普利茨矩阵");
+        }
+
+        static bool Judge(int [,]array)
+        {
+            for (var i = 0; i < array.GetLength(0); i++)
+            {
+                for (var j = 0; j < array.GetLength(1); j++)
+                {
+                    if (i != 0 && j != 0 && array[i,j] != array[i - 1,j - 1])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ homework2/questionThree/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace questionThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int top1 = -1, top2 = -1;
+            int[] array1 = new int[100];
+            int[] array2 = new int[100];
+            for(int i=2;i<=100;i++)
+            {
+                top1++;
+                array1[top1] = i;
+            }
+            for (int j = 2;j<100;j++)
+            {
+                while (top1!=-1)
+                {
+                    if (array1[top1] % j == 0 && array1[top1] == j)
+                    {
+                        top2++;
+                        array2[top2] = array1[top1];
+                        top1--;
+                    }
+                    else if (array1[top1] % j != 0)
+                    {
+                        top2++;
+                        array2[top2] = array1[top1];
+                        top1--;
+                    }
+                    else
+                        top1--;
+                }
+                int length = top2;
+                for (int k = 0; k < length; k++)
+                {
+                    top1++;
+                    array1[top1] = array2[top2];
+                    top2--;
+                }
+            }
+            Console.WriteLine("素数有：");
+            for(int j=1; j <top1+2;j++)
+            {
+                Console.Write($"{array1[j-1]}\t");
+                if (j % 5 == 0)
+                    Console.Write("\n");
+            }
+        }
+    }
+}
