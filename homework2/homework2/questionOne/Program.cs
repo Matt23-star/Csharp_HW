0d6066a3eaddccba314db698f5d82968eb43c6bd
Matt23-star <825256547@qq.com>
2021-03-12 10:13:41 +0800

    代码更新

---
+++ homework2/homework2/questionOne/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace questionOne
+{
+    class Program
+    {
+        void judge(int n)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一个数");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n; i++)
+            {
+                while (n % i == 0)
+                {
+                    Console.Write($"{i}\t");
+                    n /= i;
+                }
+            }
+        }
+    }
+}
