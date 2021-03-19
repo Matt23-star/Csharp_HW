0a6c494d16d69ab0e086607775582f8c1bbde60e
MattFH <2019302100016@whu.edu.cn>
2021-03-19 09:20:02 +0800

    update homework4/questionTwo/ClockEventArgs.cs.

--- homework4/questionTwo/ClockEventArgs.cs
+++ homework4/questionTwo/ClockEventArgs.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,7 +9,6 @@ namespace questionTwo
         public int Second { get; set; }
         public int Minute { get; set; }
         public int Hour { get; set; }
-
         public int Day { get; set; }
 
         public ClockEventArgs()
